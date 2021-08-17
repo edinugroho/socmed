279f34105b1fab2e71c8eb8d4699d032b03651c8
edinugroho <edidwinugroho80@gmail.com>
2021-08-17 13:55:03 +0700

    HashtagPost find when invalid respond nil

--- app/models/hashtag_post.rb
+++ app/models/hashtag_post.rb
@@ -1,13 +1,25 @@
 require_relative '../db/db_connector.rb'
 
 class HashtagPost
-    attr_accessor :post_id, :hashtag_id
+    attr_accessor :id, :post_id, :hashtag_id
 
     def save
         return false unless valid? 
         client = create_db_client
         client.query("insert into hashtag_posts (post_id,hashtag_id) values ('#{@post_id}','#{@hashtag_id}')")
         true
+    end
+
+    def find(id)
+        client = create_db_client
+        query_results = client.query("select * from hashtags where id = '#{id}'")
+        if query_results == nil
+            nil
+        else
+            @id = query_results.id
+            @name = query_results.name
+            self
+        end 
     end
 
     def valid?
--- spec/models/hashtag_post_spec.rb
+++ spec/models/hashtag_post_spec.rb
@@ -59,4 +59,20 @@ describe HashtagPost do
             end
         end
     end
+    
+    describe '#find' do
+        context 'when invalid' do
+            it 'respond nil' do
+                hashtag_post = HashtagPost.new
+                hashtag_post.id = 1
+
+                mock_client = double
+                allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+                expect(mock_client).to receive(:query).with("select * from hashtags where id = '#{hashtag_post.id}'").and_return(nil)
+                response = hashtag_post.find(hashtag_post.id)
+
+                expect(response).to eq(nil)
+            end
+        end
+    end
 end
