18fc66f40cf848c7aef13e9c836e03d7674bf1ee
edinugroho <edidwinugroho80@gmail.com>
2021-08-20 03:57:56 +0700

    UserController store when invalid respond false if username nil

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -13,6 +13,19 @@ describe UserController do
     end
 
     describe '#store' do
+        context 'when invalid' do
+            it 'respond false if username nil' do
+                user_controller = UserController.new
+                user = { 
+                    :email => 'edi@mail.co',
+                    :bio => 'This is bio'
+                }
+                response = user_controller.store(user.to_json)
+
+                expect(response).to eq(false.to_json)
+            end
+        end
+
         context 'when valid' do
             it 'respond true' do
                 user_controller = UserController.new
