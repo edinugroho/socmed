3e6d569aa9784aee92953f48ee38d419b2595382
edinugroho <edidwinugroho80@gmail.com>
2021-08-20 03:44:51 +0700

    refactor:UserController index when valid respond json all users

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -5,26 +5,9 @@ describe UserController do
         context 'when valid' do
             it 'respond json all users' do
                 user_controller = UserController.new
-                users = [
-                    {
-                        :id => 1, 
-                        :username => 'edi', 
-                        :email => 'edi@mail.co', 
-                        :created_at => "2021-08-18 03:30:13 +0700", 
-                        :updated_at => "2021-08-18 03:30:13 +0700"
-                    },
-                    {
-                        :id => 1, 
-                        :username => 'dwi', 
-                        :email => 'dwi@mail.co', 
-                        :created_at => "2021-08-18 03:30:13 +0700", 
-                        :updated_at => "2021-08-18 03:30:13 +0700"
-                    }
-                ]
-
-                allow(user_controller).to receive(:index).and_return(users.to_json)
-
-                expect(user_controller.index).to eq(users.to_json)
+                response = user_controller.index
+                
+                expect(user_controller.index).to eq(response)
             end
         end
     end
