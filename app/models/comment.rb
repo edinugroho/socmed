fb623caac9d1a46bc0d77ec1524ea5c544c54a2d
edinugroho <edidwinugroho80@gmail.com>
2021-08-25 05:35:54 +0700

    CommentController store when valid respond true if contain hashtag

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 require_relative '../db/db_connector.rb'
+require_relative '../../app/models/hashtag.rb'
+require_relative '../../app/models/comment_hashtag.rb'
 
 class Comment
     attr_accessor :user_id, :post_id, :body, :attachment
@@ -6,6 +8,17 @@ class Comment
     def save
         client = create_db_client
         client.query("insert into comments (user_id, post_id, body) values (#{@user_id}, #{@post_id}, '#{@body}')")
+        words = @body.split(' ')
+        words.each do |word|
+            hashtag = Hashtag.new
+            comment_hashtag = CommentHashtag.new
+            hashtag.name = word
+            if hashtag.valid?
+                comment_hashtag.comment_id = client.last_id
+                comment_hashtag.hashtag_id = hashtag.save
+                comment_hashtag.save
+            end
+        end
         true
     end
 end
--- spec/controllers/comment_controller_spec.rb
+++ spec/controllers/comment_controller_spec.rb
@@ -19,6 +19,22 @@ describe CommentController do
 
                 expect(response).to eq(true.to_json)
             end
+            
+            it 'respond true if contain hashtag' do
+                comment_controller = CommentController.new
+                comment = {
+                    "post_id" => 1,
+                    "user_id" => 1,
+                    "body" => 'Lorem ipsum #comment',
+                    'attachment' => {
+                        'filename' => 'filename.jpg',
+                        'tempfile' => Tempfile.new
+                    }
+                }
+                response = comment_controller.store(comment)
+
+                expect(response).to eq(true.to_json)
+            end
         end
     end
 end
